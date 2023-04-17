45e4487ad6ff488087d3880f6055a28884658875
p32929 <p32929@yahoo.com>
2023-04-17 13:18:05 +0600

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { readFileSync } from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -9,5 +10,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const jjj = JSON.parse(readFileSync("./test.json").toString())
+  res.status(200).json(jjj)
 }
---
+++ pages/api/save.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { writeFileSync } from 'fs'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    name: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    writeFileSync("./test.json", JSON.stringify({ name: "Fay" }))
+    res.status(200).json({ name: 'Done' })
+}
